25ab3d33d18f415d75ccda0fad7390e7a7e7fc66
Khasanov <xasanovnurilloh07@gmail.com>
2026-04-14 21:28:34 +0500

    Add remove button and finish the project
    finally the project is finished and  the remove button is added with array method called splice().

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function addBooktoLibrary(book) {
   myLibrary.push(book);
 }
 
-
 const submitBtn = document.querySelector("#submit");
 
 const titleInput = document.querySelector("#title");
@@ -20,12 +19,12 @@ const pagesInput = document.querySelector("#pages");
 const readInput = document.querySelector("#read");
 
 submitBtn.addEventListener("click", (e) => {
-  e.preventDefault(); // important if inside form
+  e.preventDefault();
 
   const title = titleInput.value;
   const author = authorInput.value;
   const pages = Number(pagesInput.value);
-  const read = readInput.checked; // better for checkbox
+  const read = readInput.checked;
 
   const newBook = new Book(title, author, pages, read);
 
@@ -33,20 +32,60 @@ submitBtn.addEventListener("click", (e) => {
 
   console.log(myLibrary);
 
-
   displayBooks();
 });
 
+function displayBooks() {
+  const container = document.querySelector("#container");
 
-function displayBooks(){
-  const container=document.querySelector("#container");
-container.innerHTML=" ";
-  myLibrary.forEach((book) =>{
-    const bookdiv=document.createElement("div");
-bookdiv.classList.add("booksCont");
-bookdiv.textContent=
-`${book.title} by ${book.author}, ${book.pages} pages`
-container.appendChild(bookdiv);
+  container.innerHTML = "";
+
+  myLibrary.forEach((book, index) => {
+    const bookdiv = document.createElement("div");
+
+    bookdiv.classList.add("booksCont");
+
+    bookdiv.textContent =
+      `"${book.title}" by ${book.author}, ${book.pages} pages`;
+
+    const readBtn = document.createElement("button");
+
+    readBtn.classList.add("readBtn");
+
+    readBtn.textContent = book.read ? "Read" : "Not Read";
+
+    if (book.read) {
+  readBtn.style.backgroundColor = "green";
+}
+else {
+  readBtn.style.backgroundColor = "red";
+}
+
+    bookdiv.appendChild(readBtn);
+
+    container.appendChild(bookdiv);
+
+    readBtn.addEventListener("click", () => {
+      book.read = !book.read;
+
+      
+      displayBooks();
+    });
+
+    const removeBtn=document.createElement("button");
+      removeBtn.classList.add("removeBtn");
+      removeBtn.textContent=`Remove this book`
+
+      removeBtn.addEventListener(("click"), ()=>{
+        if (confirm("Are you sure you want to proceed?")) {
+    myLibrary.splice(index,1);
+    displayBooks();
+}
+
+      })
+bookdiv.appendChild(removeBtn);
   });
-  
-} ;
+
+ 
+
+}
